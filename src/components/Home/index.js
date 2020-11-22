e7378888723067f51121bca1321ef71b5feb0274
maiconm <maiconm23@gmail.com>
2020-11-22 11:22:15 -0300

    Introduz conteudo inicial e estilo do compenente `Home`.

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+function Home() {
+  return (
+    <div className="center home-bg-green">
+      <h1 id="home-logo">
+        conduit
+      </h1>
+      <p id="home-lead">
+        Um lugar para voce compartilhar todo o seu conhecimento!
+      </p>
+    </div>
+  )
+}
+
+export default Home
