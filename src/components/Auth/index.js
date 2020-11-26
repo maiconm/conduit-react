8f726203669ea9253f97d25e2805bd336a366a38
maiconm <maiconm23@gmail.com>
2020-11-26 14:50:39 -0300

    auth

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -2,7 +2,27 @@ import './style.css'
 
 function Auth() {
   return (
-    'auth works!'
+    <div className="container">
+      <h1>Login</h1>
+      <div className="auth-container">
+        <form>
+          <input
+            autoFocus
+            id="email"
+            placeholder="E-mail"
+            type="text"
+          />
+          <input
+            id="password"
+            type="password"
+            name="password"
+          />
+          <button
+            type="submit"
+          >Enviar</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
